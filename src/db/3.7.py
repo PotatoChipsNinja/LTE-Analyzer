de1547f87ae931f94463b317b97ac14c34fea40b
zxy91693 <zxy91693@163.com>
2021-06-14 19:52:42 +0800

    modified 3.7.py

--- src/db/3.7.py
+++ src/db/3.7.py
@@ -28,21 +28,6 @@ def select_triplet_from_tbc2inew(xValue):
     dfData = DataFrame(list(set_allTriplet), columns={0: 'a小区ID', 1: 'b小区ID', 2: 'c小区ID'})
     return dfData
 
-
-# 测试：
-#           "with temp(TimeStamp, ServingSector, InterferingSector, LteScRSRP, LteNcRSRP, Difference, Nine, Six) as " \
-#           "(select TimeStamp, ServingSector, InterferingSector, LteScRSRP, LteNcRSRP, (LteScRSRP-LteNcRSRP) as Difference, " \
-#           "IF(LteScRSRP-LteNcRSRP< 9, 1, 0) as Nine, IF(ABS(LteScRSRP-LteNcRSRP) < 6, 1, 0) as Six from tbmro), "\
-#           "Ans(ServingSector, InterferingSector, cnt, mean, std, PrbC2I9, PrbABS6, cntNine, cntSix)" \
-#           "as (select ServingSector, InterferingSector, count(*) as cnt, AVG(LteScRSRP-LteNcRSRP) as mean, " \
-#           "STDDEV(LteScRSRP-LteNcRSRP) as std, SUM(Nine)/count(*) as PrbC2I9, SUM(Six)/count(*) as PrbABS6, " \
-#           "SUM(Nine) as cntNine, SUM(Six) as cntSix "\
-#           "from temp group by ServingSector, InterferingSector having count(*) > "+str(num)+")" \
-#           "select ServingSector, InterferingSector, mean, std, PrbC2I9, PrbABS6 from Ans"
-
 a = select_triplet_from_tbc2inew(0.002)
-
-
 print(a)
 
-# print(a['ServingSector'].tolist())
